a90f65869a575fc63eec3538f4448ed55f583eac
JIANGWEILONGGG <william@jwlchina.cn>
2018-02-13 16:18:49 +0800

    指定referrer

--- index.py
+++ index.py
@@ -29,20 +29,40 @@ class Crawler:
     def __init__(self, t=0.1):
         self.time_sleep = t
 
-    # 保存图片
-    def __save_image(self, rsp_data, word):
+    # 获取后缀名
+    def get_suffix(self, name):
+        m = re.search(r'\.[^\.]*$', name)
+        if m.group(0) and len(m.group(0)) <= 5:
+            return m.group(0)
+        else:
+            return '.jpeg'
 
+    # 获取referrer，用于生成referrer
+    def get_referrer(self, url):
+        par = urllib.parse.urlparse(url)
+        if par.scheme:
+            return par.scheme + '://' + par.netloc
+        else:
+            return par.netloc
+
+        # 保存图片
+    def save_image(self, rsp_data, word):
         if not os.path.exists("./" + word):
             os.mkdir("./" + word)
         # 判断名字是否重复，获取图片长度
         self.__counter = len(os.listdir('./' + word)) + 1
         for image_info in rsp_data['imgs']:
+
             try:
                 time.sleep(self.time_sleep)
-                suffix = self.__get_suffix(image_info['objURL'])
-                # 指定UA，减少403
+                suffix = self.get_suffix(image_info['objURL'])
+                # 指定UA和referrer，减少403
+                refer = self.get_referrer(image_info['objURL'])
                 opener = urllib.request.build_opener()
-                opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:55.0) Gecko/20100101 Firefox/55.0')]
+                opener.addheaders = [
+                    ('User-agent', 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:55.0) Gecko/20100101 Firefox/55.0'),
+                    ('Referer', refer)
+                ]
                 urllib.request.install_opener(opener)
                 # 保存图片
                 urllib.request.urlretrieve(image_info['objURL'], './' + word + '/' + str(self.__counter) + str(suffix))
@@ -59,16 +79,8 @@ class Crawler:
                 self.__counter += 1
         return
 
-    # 获取后缀名
-    def __get_suffix(self, name):
-        m = re.search(r'\.[^\.]*$', name)
-        if m.group(0) and len(m.group(0)) <= 5:
-            return m.group(0)
-        else:
-            return '.jpeg'
-
     # 开始获取
-    def __get_images(self, word='美女'):
+    def get_images(self, word='美女'):
         search = urllib.parse.quote(word)
         # pn int 图片数
         pn = self.__start_amount
@@ -94,7 +106,7 @@ class Crawler:
             else:
                 # 解析json
                 rsp_data = json.loads(rsp)
-                self.__save_image(rsp_data, word)
+                self.save_image(rsp_data, word)
                 # 读取下一页
                 print("下载下一页")
                 pn += 60
@@ -113,12 +125,12 @@ class Crawler:
         """
         self.__start_amount = (start_page - 1) * 60
         self.__amount = spider_page_num * 60 + self.__start_amount
-        self.__get_images(word)
+        self.get_images(word)
 
 
 if __name__ == '__main__':
     crawler = Crawler(0.05)  # 抓取延迟为 0.05
 
-    # crawler.start('美女', 1, 2)  # 抓取关键词为 “美女”，总数为 1 页（即总共 1*60=60 张），开始页码为 2
+    # crawler.start('美女', 10, 2)  # 抓取关键词为 “美女”，总数为 1 页（即总共 1*60=60 张），开始页码为 2
     crawler.start('二次元 美女', 10, 1)  # 抓取关键词为 “二次元 美女”，总数为 10 页（即总共 10*60=600 张），起始抓取的页码为 1
     # crawler.start('帅哥', 5)  # 抓取关键词为 “帅哥”，总数为 5 页（即总共 5*60=300 张）
