9eb045fd2c685c9631c51f4a2d5b83a14c83976a
xianxuhua <1160576878@qq.com>
2022-04-23 12:18:47 +0800

    fix json parse error

--- crawling.py
+++ crawling.py
@@ -122,7 +122,7 @@ class Crawler:
                 print("-----socket timout:", url)
             else:
                 # 解析json
-                rsp_data = json.loads(rsp, strict=False)
+                rsp_data = json.loads(rsp.decode("utf8").replace("\'", "\""))
                 if 'data' not in rsp_data:
                     print("触发了反爬机制，自动重试！")
                 else:
